3cc13a00ba9d25efdbd8713c8ca33fdbca1f41fc
Karan Kumar <karan.kumar@espire.com>
2023-03-04 17:52:34 +0000

    code refactor

---
+++ src/Project/SitecoreHackathon23/Constant.cs
@@ -0,0 +1,35 @@
+﻿namespace SitecoreHackathon23
+{
+    public class Constant
+    {
+        public static readonly string ApprovedResponse = "Approved response";
+        public static readonly string CookiesCreated = "Cookies created";
+
+        public static readonly string AuthenticateResponse = "Authenticate response";
+        public static readonly string RejectedResponse = "Rejected response";
+        public static readonly string RejectedCookies = "Rejected cookies";
+        public static readonly string AuthenticateCookies = "Authenticate cookies";
+
+        public class MethodBase
+        {
+            public static readonly string POST = "POST";
+            public static readonly string PATCH = "PATCH";
+        }
+        public class Header
+        {
+            public static readonly string Application_Json = "application/json";
+        }
+        public class EnvVariable
+        {
+            public static readonly string XMCloudDomainUrl = "XM_Cloud_Domain_Url";
+            public static readonly string XMCloudAdminUsername = "XM_Cloud_Admin_Username";
+            public static readonly string XMCloudAdminPassword = "XM_Cloud_Admin_Password";
+        }
+
+        public class Message
+        {
+            public static readonly string ItemNotFound = "Item not found."; 
+            public static readonly string ItemUpdatedSuccessfully = "Item with id {0} updated successfully.";
+        }
+    }
+}
--- src/Project/SitecoreHackathon23/Reject.cs
+++ src/Project/SitecoreHackathon23/Reject.cs
@@ -8,16 +8,15 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net;
+using SitecoreHackathon23.Service;
+using SitecoreHackathon23.Model;
 
 namespace SitecoreHackathon23
 {
-    public static class Reject
+    public class Reject : ItemService
     {
-        private static readonly string xmCloudURL = Environment.GetEnvironmentVariable("XM_Cloud_Domain_Url");
-        private static readonly string xmCloudAdminUserName = Environment.GetEnvironmentVariable("XM_Cloud_Admin_Username");
-        private static readonly string xmCloudAdminPassword = Environment.GetEnvironmentVariable("XM_Cloud_Admin_Password");
         [FunctionName("Reject")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
@@ -25,82 +24,23 @@ namespace SitecoreHackathon23
             string itemID = req.Query["itemid"];
 
             //authenticate environment
-            Resp data = Authenticate();
+            ResponseModel data = Authenticate();
 
             //approve item
-            var resp = RejectWorkflow(data, itemID);
+            var response = RejectWorkflow(data, itemID);
 
+            await new StreamReader(req.Body).ReadToEndAsync();
 
-
-
-
-
-
-
-
-
-
-
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data1 = JsonConvert.DeserializeObject(requestBody);
-
-
-            string responseMessage = string.IsNullOrEmpty(itemID)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {itemID}. This HTTP triggered function executed successfully.";
+            string responseMessage = string.IsNullOrEmpty(itemID) ? Constant.Message.ItemNotFound : string.Format(Constant.Message.ItemUpdatedSuccessfully, itemID);
 
             return new OkObjectResult(responseMessage);
         }
 
-        private static Resp RequestAPICall(string requestMethod, string url, string body, Resp resp = null)
+        private ResponseModel RejectWorkflow(ResponseModel authenticateResponse, string itemid)
         {
-            string jsonResponse = string.Empty;
-            CookieContainer cookies = new CookieContainer();
-            HttpWebRequest web = (HttpWebRequest)WebRequest.Create(url);
-            web.AllowAutoRedirect = false;
-            web.Method = requestMethod;
-            web.ContentType = "application/json";
-            web.CookieContainer = cookies;
-            if (resp != null)
-                web.CookieContainer.Add(resp.Cookies);
-            using (var streamWriter = new StreamWriter(web.GetRequestStream()))
-            {
-                string json = body;
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
-            HttpWebResponse response = (HttpWebResponse)web.GetResponse();
-            var responseCookies = response.Cookies;//cookies.GetCookies(new Uri(url)).Cast<Cookie>();
-            //jsonResponse = GetJSONResponse(response);            
-            return new Resp { Cookies = responseCookies, StatusCode = response.StatusCode };
-        }
-
-        public class Resp
-        {
-            public HttpStatusCode StatusCode { get; set; }
-            public CookieCollection Cookies { get; set; }
-        }
-        private static Resp ApproveWorkFlow(Resp authenticateResponse, string itemid)
-        {
-            var data = RequestAPICall("PATCH", $"{xmCloudURL}/sitecore/api/ssc/item/{itemid}?database=master", "{\"__Workflow state\": \"{FCA998C5-0CC3-4F91-94D8-0A4E6CAECE88}\"}", authenticateResponse);
-            Console.WriteLine($"Approved Response: {data.StatusCode}");
-            Console.WriteLine($"Approved Cookies: {data.Cookies.Count}");
-            return data;
-        }
-        private static Resp Authenticate()
-        {
-            var data = RequestAPICall("POST", $"{xmCloudURL}/sitecore/api/ssc/auth/login", "{\"domain\":\"sitecore\", \"username\":\"" + xmCloudAdminUserName + "\", \"password\":\"" + xmCloudAdminPassword + "\"}");
-            Console.WriteLine($"Authenticate Response: {data.StatusCode}");
-            Console.WriteLine($"Authenticate Cookies: {data.Cookies.Count}");
-            return data;
-        }
-        private static Resp RejectWorkflow(Resp authenticateResponse, string itemid)
-        {
-            var data = RequestAPICall("PATCH", $"{xmCloudURL}/sitecore/api/ssc/item/{itemid}?database=master", "{\"__Workflow state\": \"{190B1C84-F1BE-47ED-AA41-F42193D9C8FC}\"}", authenticateResponse);
-            Console.WriteLine($"Rejected Response: {data.StatusCode}");
-            Console.WriteLine($"Rejected Cookies: {data.Cookies.Count}");
+            var data = RequestAPICall(Constant.MethodBase.PATCH, $"{xmCloudURL}/sitecore/api/ssc/item/{itemid}?database=master", "{\"__Workflow state\": \"{190B1C84-F1BE-47ED-AA41-F42193D9C8FC}\"}", authenticateResponse);
+            Console.WriteLine($"{Constant.RejectedResponse}: {data.StatusCode}");
+            Console.WriteLine($"{Constant.RejectedCookies}: {data.Cookies.Count}");
             return data;
         }
     }
